65dee23e924330b9a3b4800d2bbca171658db0e3
山崎浩毅 <yamazakihiroki@yamazakihiroshitakeshi-no-MacBook-Pro.local>
2019-05-26 19:47:01 +0900

    initial commit

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        // optional binding(if you can downcast this data into UIImage, then you should execute below.)
+        // Using optional binding makes our code safer, more readable, and more explicit.
+        if let userPickedimage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedimage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+        
+    }
+    
 }
 
